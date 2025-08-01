3ba65663412b689a880d2b7bc2c955657bb5385f
arran B) <arranh2302@hotmail.com>
2025-08-01 17:36:22 +0100

    Add files via upload

---
+++ source/rolequotesender.py
@@ -0,0 +1,43 @@
+# this one pings a role b4 the embed
+# change the __'s in "<@&__>" and replace it with your role ID
+# thanks
+
+import time
+import random
+import requests
+
+WEBHOOK_URL = "--"
+
+quotes = [
+    "Jesus Christ says you should download [my software](https://github.com/Epicinver)",
+    "Let there be [Python](https://python.org), let there be [Visual Basic](https://visualstudio.com), let there be EVERYTHING",
+    "uwu",
+    "He said: push to main, and [GitHub](https://github.com/) approved.",
+    "Ohio skibidi :3"
+]
+
+def send_quote():
+    content = "<@&__>" 
+    payload = {
+        "content": content,
+        "embeds": [
+            {
+                "title": "Quote of the day:",
+                "description": random.choice(quotes),
+                "color": 0xD41D81,
+                "footer": {
+                    "text": "the holy quote sender - 69/69/6969, 69:69 -  ;)"
+                }
+            }
+        ]
+    }
+
+    response = requests.post(WEBHOOK_URL, json=payload)
+    if response.status_code == 204 or response.status_code == 200:
+        print("✨ Quote sent!")
+    else:
+        print(f"⚠ Failed to send: {response.status_code} - {response.text}")
+
+while True:
+    send_quote()
+    time.sleep(800)  # 13 minutes 20 seconds
